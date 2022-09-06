a2b9b310fc7a550e836751ba8a55a9d27f268b4d
shyaka axel divin <Xldivin@gmail.com>
2022-09-06 23:44:37 +0200

    fx(main)2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const button = document.getElementById('tap');
 const div = document.querySelector('.show');
+const menu = document.querySelector('#menu-icon');
+const menuIcon = document.querySelector('.navMobile');
+const menuRemover = document.querySelector('#menu-icon1');
+const navbar = document.querySelector('.MobileNavbar');
 
 button.addEventListener('click',() =>{
     if(div.style.display == 'none'){
@@ -12,9 +16,7 @@ button.addEventListener('click',() =>{
 
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 2,
-    loop:true,
+    slidesPerView: screen.width<600 ? 1 :2,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -24,3 +26,19 @@ var swiper = new Swiper(".mySwiper", {
         hide: true,
     },
   });
+
+
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menuIcon.classList.toggle('off')
+
+}
+menuRemover.onclick = () => {
+    navbar.classList.toggle('active');
+    menuIcon.classList.toggle('off')
+
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
